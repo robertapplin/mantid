90cf4a298b1e0dc0b6a1adba78493835a16877ad
Robert Applin <robert.applin@stfc.ac.uk>
2021-02-22 09:24:01 +0000

    Refs #29892. Make GUI appearance changes.

--- qt/widgets/common/src/FunctionMultiDomainPresenter.cpp
+++ qt/widgets/common/src/FunctionMultiDomainPresenter.cpp
@@ -338,10 +338,17 @@ void FunctionMultiDomainPresenter::clear() {
   m_view->clear();
   emit functionStructureChanged();
 }
+
 void FunctionMultiDomainPresenter::setColumnSizes(int s0, int s1, int s2) {
   auto treeView = dynamic_cast<FunctionTreeView *>(m_view);
   if (treeView)
     treeView->setColumnSizes(s0, s1, s2);
+}
+
+void FunctionMultiDomainPresenter::setStretchLastColumn(bool stretch) {
+  auto treeView = dynamic_cast<FunctionTreeView *>(m_view);
+  if (treeView)
+    treeView->setStretchLastColumn(stretch);
 }
 
 void FunctionMultiDomainPresenter::setErrorsEnabled(bool enabled) {
